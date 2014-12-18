6365d882130195cb95ad290b9e0593add273deb6
Glenn Meyer <glennmeyer@gmail.com>
2014-12-18 14:39:33 -0600

    fix error

--- lib/repos/sessions_repo.rb
+++ lib/repos/sessions_repo.rb
@@ -3,6 +3,7 @@ module RpsGame
     def self.create_session(db, user_id)
       session_id = SecureRandom.hex(16)
       db.exec('INSERT INTO sessions (user_id, session_id) VALUES ($1, $2) RETURNING user_id, session_id', [user_id, session_id]).first
+    end
 
     def self.end_session(db, user_data)
       db.exec('DELETE FROM sessions WHERE user_id = $1 AND session_id = $2', [user_data['id'], user_data['session_id']])
