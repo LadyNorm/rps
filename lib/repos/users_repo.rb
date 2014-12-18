38aafa27722695c1d86751a1d65b57670dfc8423
David Coleman <colemandavid55@gmail.com>
2014-12-18 09:50:02 -0600

    begin users_repo code

--- lib/repos/users_repo.rb
+++ lib/repos/users_repo.rb
@@ -2,6 +2,10 @@ require 'securerandom'
 
 module RpsGame
   class UsersRepo
-
+    def self.find db, user_id
+      sql = %q[SELECT * FROM users WHERE id = $1]
+      result = db.exec(sql, [user_id])
+      result.first
+    end
   end
 end
