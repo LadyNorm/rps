68035e31fdc2a4a9a48e88e3f336765ad6d5d2a4
David Coleman <colemandavid55@gmail.com>
2014-12-18 10:31:16 -0600

    add first user_repo test

--- spec/repos/users_repo.rb
+++ spec/repos/users_repo.rb
@@ -1,3 +1,5 @@
+require 'spec_helper'
+
 describe RpsGame::UsersRepo do
 
   def user_count(db)
@@ -9,6 +11,28 @@ describe RpsGame::UsersRepo do
   before(:each) do
     RpsGame.clear_tables(db)
   end
+
+  it "gets all users" do
+    db.exec("INSERT INTO users (name, password, score) VALUES ($1, $2, $3)", ["Alice", "password_1", 4])
+    db.exec("INSERT INTO users (name, password, score) VALUES ($1, $2, $3)", ["Bob", "password_2", -2])
+
+    users = RpsGame::UsersRepo.all(db)
+    expect(users).to be_a Array
+    exepect(users.count).to eq 2
+
+    usernames = users.map { |u| u['username'] }
+    expect(usernames).to include "Alice", "Bob"
+    
+    passwords = users.map { |u| u['password'] }
+    exepect(passwords).to include "password_1", "password_2"
+    
+    scores = users.map { |u| u['score'] }
+    expect(scores).to include 4, -2 
+  end
+
+  # xit "finds a user" do
+  #   user = RpsGame::UsersRepo.sign_in(db, { 'username' => 'Alice'} )
+  #   retrieved_user = RpsGame::UsersRepo.find(db, user['id'] )
 
 
 end
